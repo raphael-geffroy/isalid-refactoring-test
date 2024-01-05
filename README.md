f7d6a4b29889172c3e3858a42677a3a26d3d03cd
raphael-geffroy <raphael@geffroy.dev>
2024-01-05 15:40:49 +0100

    Add approval tests

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Refactoring Kata Test
+
+## 1 - Understand the code
+We create Templates with subjects and contents, and we use the TemplateManager
+to compute those templates (replace placeholders with real data where needed).
+Placeholders can relate to a given quote or a user.
+User and Quote are passed to TemplateManager through a parameter.
+If a user is not provided it will fetch the connected User through the ApplicationContext.
+
+## 2 - Protect the code with tests
+I chose to cover the code with approval testing.
+It is specially recommended for this type of situation when we want to be sure not to introduce
+behavioral changes while refactoring procedural legacy code which is not tested enough.
+Those tests are to be thrown away once the refactoring is done.
+(I had to use a fork branch of the official library because it was not compatible with PHP5.6)
+Once the code to refactor is covered enough we save the snapshot, and we are good to start refactoring.
---
+++ tests/TemplateManagerApprovalTest.php
@@ -0,0 +1,107 @@
+<?php
+
+require_once __DIR__ . '/../src/Entity/Destination.php';
+require_once __DIR__ . '/../src/Entity/Quote.php';
+require_once __DIR__ . '/../src/Entity/Site.php';
+require_once __DIR__ . '/../src/Entity/Template.php';
+require_once __DIR__ . '/../src/Entity/User.php';
+require_once __DIR__ . '/../src/Helper/SingletonTrait.php';
+require_once __DIR__ . '/../src/Context/ApplicationContext.php';
+require_once __DIR__ . '/../src/Repository/Repository.php';
+require_once __DIR__ . '/../src/Repository/DestinationRepository.php';
+require_once __DIR__ . '/../src/Repository/QuoteRepository.php';
+require_once __DIR__ . '/../src/Repository/SiteRepository.php';
+require_once __DIR__ . '/../src/TemplateManager.php';
+
+class TemplateManagerApprovalTest extends PHPUnit_Framework_TestCase
+{
+    const QUOTE_ID = 1;
+    const QUOTE_DATE = '2023-01-01T08:00:00Z';
+
+    const SITE_ID = 2;
+    const DESTINATION_ID = 3;
+    const USER_ID = 4;
+
+    const TEMPLATE_ID = 5;
+    const TEMPLATE_SUBJECT = 'Votre livraison à [quote:destination_name]';
+    const TEMPLATE_CONTENT = <<<EOT
+Bla [user:first_name]
+blabla [quote:destination_name]
+blablabla [quote:destination_link]
+blablablabla [quote:summary_html]
+blablablablabla [quote:summary]
+EOT;
+
+    public function test()
+    {
+        \ApprovalTests\CombinationApprovals::verifyAllCombinations3(
+            [$this, 'callTemplateManagerWithParams'],
+            [self::TEMPLATE_CONTENT],
+            [true, false],
+            [true, false]
+        );
+    }
+
+    public function callTemplateManagerWithParams($content, $withQuote, $withUser)
+    {
+        $templateManager = new TemplateManager();
+
+        $context = [];
+        if ($withQuote) {
+            $quote = new Quote(
+                self::QUOTE_ID,
+                self::SITE_ID,
+                self::DESTINATION_ID,
+                new DateTimeImmutable(self::QUOTE_DATE)
+            );
+            $context['quote'] = $quote;
+        }
+        if ($withUser) {
+            $user = new User(self::USER_ID, 'Tryphon', 'Tournesol', 'tryphon.tournesol@gmail.com');
+            $context['user'] = $user;
+        }
+
+        $template = new Template(self::TEMPLATE_ID, self::TEMPLATE_SUBJECT, $content);
+
+        $result = $templateManager->getTemplateComputed($template, $context);
+
+        /*
+         * We need to scrub the result because when we do not provide a user
+         * it takes it from the ApplicationContext which is an unseeded faked value
+         */
+        return $this->scrub(
+            $this->printTemplate($result),
+            '/(?<=Bla )[a-zA-Z]+/',
+            'FIRSTNAME_'
+        );
+    }
+
+    /**
+     * @param Template $template
+     * @return string
+     */
+    private function printTemplate($template)
+    {
+        return sprintf('{"id": "%s","subject": "%s","content": "%s"}', $template->id, $template->subject, $template->content);
+    }
+
+    /**
+     * @param string $text
+     * @param string $regex
+     * @param string $prefix
+     * @return string
+     */
+    private function scrub($text, $regex, $prefix)
+    {
+        preg_match_all($regex, $text, $matches);
+        $matching = $matches[0];
+
+        $matchingToId = array_flip(array_unique($matching));
+
+        $replacedByValue = array_map(function ($id) use ($prefix){
+            return $prefix . $id;
+        }, $matchingToId);
+
+        return str_replace(array_keys($replacedByValue), $replacedByValue, $text);
+    }
+}
